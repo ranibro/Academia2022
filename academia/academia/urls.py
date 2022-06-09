01a3387607185255f9ba3d6c07d08e8c4c2c9b1d
Gabriel Batista dos Santos <86369237+Gabri3lBatista@users.noreply.github.com>
2022-06-08 20:00:05 -0500

    ok

--- academia/academia/urls.py
+++ academia/academia/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from pipes import Template
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="blank.html"))
 ]
