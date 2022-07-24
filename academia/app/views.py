e6687f0c627a25bb39dbaf124d3ae3190e53714b
ranibro <ranibro@gmail.com>
2022-07-24 16:01:10 -0500

    Modificação
    Mudeis só umas coisinhas

--- academia/app/views.py
+++ academia/app/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from app.models import Clientes
+
 #definir função para refernciar os templates em urls.
 #return render(request, 'nomeDoTemplate')
 
 class ClientesCad(CreateView):
     model = Clientes
     fields = ['nome', 'email']
-    template_name = 'cadastroCliente.html'
-    
-    
+    template_name = 'cadastros/cadastroCliente.html'
 
 
 def index(request):
@@ -19,4 +18,4 @@ def base(request):
     return render(request, 'base.html')
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    return render(request, 'cadastros/cadastro.html')
