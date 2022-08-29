3072328f19740e912c7c23ede5828f48451e241c
ranibro <ranibro@gmail.com>
2022-08-28 19:34:43 -0500

    .

--- academia/apps/alunos/views.py
+++ academia/apps/alunos/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView,  UpdateView
 from .models import Clientes
 from django.views.generic.list import ListView
-from django.http import HttpResponseRedirect
-from django.template.response import TemplateResponse
-
 
 class ClientesCad(CreateView):
     model = Clientes
     fields = ['nome', 'email','password','idade', 'altura','peso']
     template_name = 'cadastros/cadastroCliente.html'
-#Tirar esse redirect aqui, no lugar disso usar uma caixa de texto de alerta
     success_url = reverse_lazy('alunos:clientes')
 
 def verificacao(request):
@@ -20,37 +15,6 @@ def verificacao(request):
            return True
     except:
         return False
- 
-def login(request):
-    if verificacao(request):
-        return HttpResponseRedirect('/')
-    
-    template_name = 'login/login.html'
-    error_message = None
-    
-    if request.method == "POST":
-        email = request.POST.get("email")
-        senha = request.POST.get("senha")
-        
-        if Clientes.objects.filter(email = email).exists():
-            if Clientes.objects.filter(email = email, senha = senha).exists():
-            
-                usuario = Clientes.objects.get(email = email, senha = senha)
-            
-                request.session['email'] = usuario.email
-                request.session['nome'] = usuario.nome
-                request.session['id'] = usuario.id
-
-                return HttpResponseRedirect('/')
-            else:
-                error_message = "Senha de acesso incorreta!"
-                return TemplateResponse(request, template_name, locals())
-        else:
-            error_message = "E-mail inválido ou não encontrado!"
-            return TemplateResponse(request, template_name, locals())
-    else:
-        return TemplateResponse(request, template_name, locals())
-
 
 class ClienteListagem(ListView):
     model = Clientes
--- academia/apps/funcionarios/views.py
+++ academia/apps/funcionarios/views.py
@@ -1,31 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
 
 def listaclientes(request):
     if request.user.is_authenticated:
-        return redirect('alunos:ClienteListagem')
+        return redirect('alunos:clientes')
     else:
         return redirect(paginalogin)
 
 def paginalogin(request):
     if request.user.is_authenticated:
-        return redirect('alunos:ClienteListagem')
+        return redirect('alunos:clientes')
     else:
         if request.method == "POST":
-            email = request.POST.get('InputEmail') #'InputEmail' in request.POST
-            senha = request.POST.get('InputPassword')#'InputPassword' in request.POST
+            email = request.POST.get('InputEmail')
+            senha = request.POST.get('InputPassword')
 
             user = authenticate(username=email, password=senha)
             if user is not None:
                 login(request, user)
-                #colocar backend?
                 return redirect('alunos:clientes')
             else:
                 messages.error(request, "Usuario ou senha invalidos!")
                 return render(request, '../templates/login/login.html')
         else:
             return render(request, '../templates/login/login.html')
-        
-        
