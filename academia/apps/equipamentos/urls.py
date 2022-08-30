7c91e3fdde3a89fb7e8f3fa30856efadd17ea4a3
Gabriel Batista dos Santos <86369237+Gabri3lBatista@users.noreply.github.com>
2022-08-29 22:03:28 -0500

    Update urls.py

--- academia/apps/equipamentos/urls.py
+++ academia/apps/equipamentos/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from ...static.img.views import EquipamentosLista, EquipamentoCad, EquipamentosUpdate
+from .views import EquipamentosLista, EquipamentoCad, EquipamentosUpdate
 from django.conf import settings
 from django.conf.urls.static import static
 app_name = 'equipamentos'
