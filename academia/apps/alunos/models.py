2492238f814d408491d5a2b42a7642f02cbdde4b
ranibro <ranibro@gmail.com>
2022-09-03 23:20:32 -0500

    #5 models de treino

--- academia/apps/alunos/models.py
+++ academia/apps/alunos/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from apps.equipamentos.models import Equipamentos
 from django.contrib.auth.models import User
 
 # Create your models here.
-
 #fazer um status de inativo ou ativo
 
 class Clientes (models.Model):
@@ -13,3 +13,17 @@ class Clientes (models.Model):
     
     def __str__(self):
         return "{} - {} - {}".format(self.nome, self.email, self.usuario)
+
+#Mover esse exercicio para outro APP depois
+class Exercicio (models.Model):
+    repetições = models.IntegerField()
+    carga = models.IntegerField()
+    serie = models.IntegerField()
+    tempo = models.IntegerField(null=True, help_text="Opcional")
+    equipamento = models.ForeignKey(Equipamentos, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}".format(self.equipamento)
+
+class Treino (models.Model):
+    nome = models.CharField('Nome do Treino', max_length=16)
+    exercicio = models.ManyToManyField(Exercicio)
