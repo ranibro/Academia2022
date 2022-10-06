096ac17350881166add3a5bde3e246b089b5441d
Gabriel Batista dos Santos <86369237+Gabri3lBatista@users.noreply.github.com>
2022-10-05 21:27:25 -0500

    Update views.py

--- academia/apps/alunos/views.py
+++ academia/apps/alunos/views.py
@@ -5,9 +5,9 @@ from django.views.generic.list import ListView
 
 class ExercicioCad(CreateView):
     model = Exercicio
-    fields = ['equipamento', 'repeticao','carga', 'serie', 'tempo']
+    fields = ['equipamento', 'repeticao','carga','tempo']
     template_name = 'cadastros/cadastroTreino.html'
-    success_url = reverse_lazy('funcionarios:Exercicio')
+    success_url = reverse_lazy('alunos:exercicios')
 
 class ExercicioListagem(ListView):
     login_url = reverse_lazy('login')
@@ -18,4 +18,4 @@ class ExercicioUpdate(UpdateView):
     model = Exercicio
     fields = "__all__"
     template_name = 'cadastros/cadastroCliente.html'
-    success_url = reverse_lazy('funcionarios:Exercicio')
+    success_url = reverse_lazy('alunos:exercicios')
