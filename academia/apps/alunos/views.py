d1d9ba14714d6737160207cdd0743170f6de7539
Gabriel Batista dos Santos <86369237+Gabri3lBatista@users.noreply.github.com>
2022-10-12 21:55:49 -0500

    alterações

--- academia/apps/alunos/views.py
+++ academia/apps/alunos/views.py
@@ -1,14 +1,14 @@
 from django import forms
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from .models import Exercicio, Treino
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
-
-class TreinoCad(CreateView):
-    model = Treino
-    fields = ['nome', 'aluno', 'exercicio', 'inicio']
-    template_name = 'cadastros/cadastroTreino.html'
-    success_url = reverse_lazy('alunos:exercicios')
+from .forms import TreinoForm
+def treinoCad(request):
+   form = TreinoForm
+   context = {'form':form}
+   return render(request, 'cadastroTreino.html', context)
 
 class ExercicioCad(CreateView):
     model = Exercicio
